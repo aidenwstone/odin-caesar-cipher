cadda946fc550be5d45dea9a4a89ce1653702395
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-11 17:07:11 -0400

    Store letters of the alphabet in constants

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,6 @@
+UPPER_ALPHABET = ('A'..'Z').to_a
+LOWER_ALPHABET = ('a'..'z').to_a
+
 def caesar_cipher(message, shift_factor)
   # Take a string and shift factor as input
   # Output the ciphered string
