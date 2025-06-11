19a425ef3e0d92dde67a7eabc8995755618db08b
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-11 17:21:22 -0400

    Implement caesar_cipher method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,4 +4,16 @@ LOWER_ALPHABET = ('a'..'z').to_a
 def caesar_cipher(message, shift_factor)
   # Take a string and shift factor as input
   # Output the ciphered string
+  ciphered_message = message.each_char.map do |char|
+    if UPPER_ALPHABET.include?(char)
+      index = UPPER_ALPHABET.index(char)
+      UPPER_ALPHABET.rotate(shift_factor)[index]
+    elsif LOWER_ALPHABET.include?(char) 
+      index = LOWER_ALPHABET.index(char)
+      LOWER_ALPHABET.rotate(shift_factor)[index]
+    else
+      char
+    end
+  end
+  ciphered_message.join
 end
