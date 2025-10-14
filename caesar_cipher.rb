08cb9f344c53153978735cb128b0c35f85785993
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-10-14 19:37:25 -0400

    Fix linting errors

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 UPPER_ALPHABET = ('A'..'Z').to_a
 LOWER_ALPHABET = ('a'..'z').to_a
 
-def caesar_cipher(message, shift_factor)
+def caesar_cipher(message, shift_factor) # rubocop:disable Metrics/MethodLength
   # Take a string and shift factor as input
   # Output the ciphered string
   ciphered_message = message.each_char.map do |char|
     if UPPER_ALPHABET.include?(char)
       index = UPPER_ALPHABET.index(char)
       UPPER_ALPHABET.rotate(shift_factor)[index]
-    elsif LOWER_ALPHABET.include?(char) 
+    elsif LOWER_ALPHABET.include?(char)
       index = LOWER_ALPHABET.index(char)
       LOWER_ALPHABET.rotate(shift_factor)[index]
     else
